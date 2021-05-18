ab5c023ec9bc9ee1d2e6aba70c1c1d63949dc555
Caterina Bertotti <bertotti4997@gmail.com>
2021-05-18 16:45:04 +0200

    Creata tabella in homepage

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -5,7 +5,24 @@
 @endsection
 
 @section('content')
-    @foreach ($movies as $movie)
-        <h2><a href="{{ route('movies.show', ['movie' => $movie->id ] ) }}">{{ $movie->title }}</a></h2>
-    @endforeach
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Titolo</th>
+            <th scope="col">Regista</th>
+            <th scope="col">Generi</th>
+            <th scope="col">Azioni</th>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach ($movies as $movie)
+                <tr>
+                    <td>{{ $movie->title }}</td>
+                    <td>{{ $movie->author }}</td>
+                    <td>{{ $movie->genre }}</td>
+                    <td><a href="{{ route('movies.show', ['movie' => $movie->id ] ) }}">Dettaglio film</a></td>
+                </tr>
+            @endforeach
+        </tbody>
+      </table>
 @endsection
