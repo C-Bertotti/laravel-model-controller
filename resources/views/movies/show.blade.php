8eeee52766c2b68b7aec7b363ef3a9bf624475af
Caterina Bertotti <bertotti4997@gmail.com>
2021-05-18 16:35:45 +0200

    Modificato struttura della tabella: aggiunta colonna year

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -1,15 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Prova</title>
-</head>
-<body>
-    <h1>Show</h1>
-    <h2>{{ $movie->title }}</h2>
-    <p><a href="{{ route('movies.index') }}">Back</a></p>
-    
-</body>
-</html>
+@extends('layouts.base')
+
+@section('pageTitle')
+    {{ $movie->title }}
+@endsection
+
+@section('content')
+    <h2>{{ $movie->year }}</h2>
+    <p>{{ $movie->plot }}</p>
+    <a href="{{ route('movies.index') }}">Back</a>
+@endsection
+
